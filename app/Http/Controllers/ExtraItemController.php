0c11ec7aebf509a636e2bb06086435fc45a285ea
QuangAnh741963 <vipgoku741@gmail.com>
2023-07-16 21:12:44 +0700

    new

--- app/Http/Controllers/ExtraItemController.php
+++ app/Http/Controllers/ExtraItemController.php
@@ -3,26 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\ExtraItem;
+use App\Traits\RespondsWithHttpStatus;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ExtraItemController extends Controller
 {
+    use RespondsWithHttpStatus;
     /**
      * GET ALL EXTRA_ITEM
      */
-    public function index()
+    public function index(Request $request)
     {
-        return ExtraItem::all();
+        $query = ExtraItem::query();
+
+        // Lấy ra Item theo tên
+        if($request->has('name')) {
+            $query->where('name', $request->get('name'));
+        }
+
+        $extra_items = $query->get();
+        return $this->success('Get Extra Item Successfully', $extra_items);
     }
 
     /**
-     * Store a newly created resource in storage.
+     *    ADD ITEM
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $extra_items = new ExtraItem();
+            $extra_items->name = $request->input('name');
+            $extra_items->price = $request->input('price');
+            $extra_items->unit = $request->input('unit');
+
+            $extra_items->save();
+            return $this->success('Add Extra Item successfully', $extra_items);
+
+        } catch (Exception $exception) {
+            return $this->failure($exception->getMessage());
+        }
     }
 
     /**
@@ -42,18 +63,42 @@ class ExtraItemController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     *     UPDATE ITEM
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $extra_items = ExtraItem::findOrFail($id);
+            $extra_items->name = $request->input('name');
+            $extra_items->price = $request->input('price');
+            $extra_items->unit = $request->input('unit');
+
+            $extra_items->save();
+            return $this->success('Extra Item updated successfully', $extra_items);
+
+        } catch (Exception $exception) {
+            if ($exception instanceof ModelNotFoundException) {
+                return $this->failure('Extra Item NOT FOUND', 404);
+            }
+            return $this->failure($exception->getMessage(), 404);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * DELETE ITEM
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $extra_items = ExtraItem::findOrFail($id);
+            $extra_items->delete();
+            return $this->success('Extra Item DELETED successfully');
+
+        } catch (Exception $exception) {
+            if ($exception instanceof ModelNotFoundException) {
+                return $this->failure('Extra Item NOT FOUND', 404);
+            }
+            return $this->failure($exception->getMessage(), 404);
+        }
     }
 }
